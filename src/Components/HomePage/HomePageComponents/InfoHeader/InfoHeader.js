b4cef075d06f8867d300eb56f52b9bdcd138f33f
Seb <sebcyde@hotmail.com>
2021-11-16 00:13:50 +0000

    added in placeholder scroll text, added in no wrap to scroll text

--- src/Components/HomePage/HomePageComponents/InfoHeader/InfoHeader.js
+++ src/Components/HomePage/HomePageComponents/InfoHeader/InfoHeader.js
@@ -46,7 +46,7 @@ function InfoHeader() {
 	return (
 		<div id="scroll-container">
 			<div id="scroll-text">
-				{Trending.map((Coins) => {
+				{/* {Trending.map((Coins) => {
 					return (
 						<span className="TrendingContainer" key={Coins}>
 							<p className="Symbol">{Coins.symbol}:</p>
@@ -56,7 +56,12 @@ function InfoHeader() {
 							</p>
 						</span>
 					);
-				})}
+				})} */}
+				It is a long established fact that a reader will be distracted by the
+				readable content of a page when looking at its layout. The point of
+				using Lorem Ipsum is that it has a more-or-less normal distribution of
+				letters, as opposed to using 'Content here, content here', making it
+				look like readable English.
 			</div>
 		</div>
 	);
